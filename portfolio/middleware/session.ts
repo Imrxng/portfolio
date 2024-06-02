8fad30ac3c4878d319c6d254262fa4fc1f03fc19
imran.ghaddoura <s148286@ap.be>
2024-06-02 21:56:06 +0000

    af

--- portfolio/middleware/session.ts
+++ portfolio/middleware/session.ts
@@ -30,4 +30,9 @@ export default session({
     store: mongoStore,
     resave: false,
     saveUninitialized: false,
+    cookie: {
+        maxAge: 60 * 60 * 24 * 365,
+        httpOnly: true,
+        sameSite: "lax"
+    }
 });
