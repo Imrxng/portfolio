9aa36ab3b8ff26a1d2d022539ec5284d62dfe61c
imrxnG <imgha@hotmail.com>
2024-10-27 22:41:39 +0000

    fix language click

--- src/components_en/LanguageIcon_en.tsx
+++ src/components_en/LanguageIcon_en.tsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { DataContext } from "../context/DataContext";
+import { circleMarker } from "leaflet";
 
 interface LanguageIconProps {
     language: string
@@ -9,8 +10,16 @@ const LANGUAGE_ICON = ({ language } : LanguageIconProps) => {
 
     const { LANGUAGE, SETLANGUAGE} = useContext(DataContext);
 
+    const HandleClick: React.MouseEventHandler<HTMLImageElement> = () => {
+        if (LANGUAGE === "nl" && language != "dutch") {
+            SETLANGUAGE("en");
+        } else if (LANGUAGE === "en" && language != "english") {
+            SETLANGUAGE("nl");
+        }
+    };
+
     return (
-        <img id="language-icon" onClick={() => LANGUAGE === "nl" ? SETLANGUAGE("en") : SETLANGUAGE("nl")} src={`./assets/icons/${language === "nl" ? "dutch" : "english" }.webp`} alt={`${language === "nl" ? "dutch" : "english" }flag`}/>
+        <img id="language-icon" onClick={HandleClick} src={`./assets/icons/${language === "nl" ? "dutch" : "english" }.webp`} alt={`${language === "nl" ? "dutch" : "english" }flag`}/>
     );
 };
 
